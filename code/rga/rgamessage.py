e4c5aae598e0c587b3b9ad636eea330e6613e4f5
Sami Abobaker <abobakersami4@gmail.com>
2026-08-18 14:29:18 +0100

    RGA lint

--- code/rga/rgamessage.py
+++ code/rga/rgamessage.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
-from unique_char.uniquechar import UniqueChar
-from device.operations import ClientInsertOperation, ClientDeleteOperation
+
+from device.operations import ClientDeleteOperation, ClientInsertOperation
 from rga.rgatree import RGATimestamp
+from unique_char.uniquechar import UniqueChar
+
 
 @dataclass
 class RGAInsertionOperation:
