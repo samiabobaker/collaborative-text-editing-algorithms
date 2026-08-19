22bdff8fb4a59db7d8b1be2bb46e6d9a45e31743
Sami Abobaker <abobakersami4@gmail.com>
2026-08-19 10:16:25 +0100

    Tests type check

--- code/convergence_checker/test_convergence_checker.py
+++ code/convergence_checker/test_convergence_checker.py
@@ -3,6 +3,7 @@ import random
 from adopted.adoptedtransform import EllisTransform
 from algorithm_setup.algorithm_setup import adopted_setup
 from convergence_checker.convergence_checker import convergence_checker
+from device.clientdevice import ClientDevice
 
 
 # Ellis's transformation rules can produce a delete one position past the end of a
@@ -11,7 +12,7 @@ def test_inapplicable_operation_reports_non_convergence():
     random.seed(55)
     server, clients = adopted_setup(EllisTransform)(3)
 
-    client_dict = {}
+    client_dict: dict[int, ClientDevice] = {}
     for client in clients:
         client_dict[client.client_id] = client
 
