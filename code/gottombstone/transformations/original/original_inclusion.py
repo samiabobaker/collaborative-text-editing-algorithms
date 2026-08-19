dd9a1aa32537ccc81f8d47ef9ac5ba2fe74b647f
Sami Abobaker <abobakersami4@gmail.com>
2026-08-19 09:58:34 +0100

    GOT tombstone typecheck

--- code/gottombstone/transformations/original/original_inclusion.py
+++ code/gottombstone/transformations/original/original_inclusion.py
@@ -47,7 +47,7 @@ class OriginalInclusionTransformer(InclusionTransformer):
 
     def _inclusion_transform(self, op_a: GOTTombstoneOperation, op_b: GOTTombstoneOperation) -> GOTTombstoneOperation:
         assert isinstance(op_a, (GOTInsertOperation, GOTDeleteOperation))
-        assert op_a._relative_addressed_to is None
+        assert op_a.relative_addressed_to is None
 
         if isinstance(op_a, GOTInsertOperation) and isinstance(op_b, GOTInsertOperation):
             out = self._it_insert_insert(op_a, op_b)
